3876e63dd03a47b56e80079ab745d7afa7e71b15
Marcos Vinicius <marcosrodrigues993@hotmail.com>
2024-10-16 15:32:19 -0300

    product entity, many-to-many and sql script

--- backend/src/main/java/com/devsuperior/catalog/entities/Product.java
+++ backend/src/main/java/com/devsuperior/catalog/entities/Product.java
@@ -1,7 +1,7 @@
 package com.devsuperior.catalog.entities;
 
 import java.io.Serializable;
-import java.time.Instant;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -11,36 +11,44 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable{
-
+@Table(name = "tb_product")
+public class Product implements Serializable {
+	
 	private static final long serialVersionUID = 1L;
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
-	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-	private Instant createdAt;
+	@Column(columnDefinition = "TEXT")
+	private String description;
+	private Double price;
+	private String imgUrl;
 	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-	private Instant updateAt;
+	private Date date;
 	
-	@ManyToMany(mappedBy = "categories")
-	private Set<Product> products = new HashSet<>();
+	@ManyToMany
+	@JoinTable(name = "tb_product_category",
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
 	
-	public Category(){
+	public Product(){
 	}
-	
-	public Category(Long id, String name) {
+
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
 	}
 
 	public Long getId() {
@@ -59,33 +67,38 @@ public class Category implements Serializable{
 		this.name = name;
 	}
 
-	public Instant getCreatedAt() {
-		return createdAt;
+	public String getDescription() {
+		return description;
 	}
 
-	public Instant getUpdateAt() {
-		return updateAt;
-	}
-	
-	public Set<Product> getProducts() {
-		return products;
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
-	@PrePersist
-	public void prePersist() {
-		createdAt = Instant.now();
+	public Double getPrice() {
+		return price;
 	}
-	
-	@PreUpdate
-	public void preUpdate() {
-		updateAt = Instant.now();
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
 	}
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-	
 
 	@Override
 	public boolean equals(Object obj) {
@@ -95,8 +108,8 @@ public class Category implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 }
